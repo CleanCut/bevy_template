ed4a8538ca7125fc9a132d11ccbc5714bdfca37e
Nathan Stocks <nathan@agileperception.com>
2021-02-16 20:37:23 -0700

    Update system conventions for Bevy 0.4.0

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@ struct Velocity(f32);
 struct Position(f32);
 
 // this system spawns entities with the Position and Velocity components
-fn setup(mut commands: Commands) {
+fn setup(commands: &mut Commands) {
     commands
         .spawn((Position(0.0), Velocity(1.0)))
         .spawn((Position(1.0), Velocity(2.0)));
 }
 
 // this system runs on each entity with a Position and Velocity component
-fn movement(mut position: Mut<Position>, velocity: &Velocity) {
-    position.0 += velocity.0;
-    println!("{}", position.0);
+fn movement(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in query.iter_mut() {
+        position.0 += velocity.0;
+        println!("{}", position.0);
+    }
 }
 
 // the app entry point
