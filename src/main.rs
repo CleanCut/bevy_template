a6ab0156ad81f43d3ab425ea7236a63286979a44
Nathan Stocks <cleancut@github.com>
2021-04-18 21:30:29 -0600

    Make the example slightly more interesting.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use bevy::prelude::*;
 
 struct Velocity(f32);
 struct Position(f32);
+#[derive(Debug)]
+enum Choice {
+    Yes,
+    No,
+}
 
 // this system spawns entities with the Position and Velocity components
 fn setup(commands: &mut Commands) {
     commands
-        .spawn((Position(0.0), Velocity(1.0)))
-        .spawn((Position(1.0), Velocity(2.0)));
+        .spawn((Position(0.0), Velocity(1.0), Choice::Yes))
+        .spawn((Position(1.0), Velocity(2.0), Choice::No));
 }
 
 // this system runs on each entity with a Position and Velocity component
-fn movement(mut query: Query<(&mut Position, &Velocity)>) {
-    for (mut position, velocity) in query.iter_mut() {
+fn movement(mut query: Query<(&mut Position, &Velocity, &Choice)>) {
+    for (mut position, velocity, choice) in query.iter_mut() {
         position.0 += velocity.0;
-        println!("{}", position.0);
+        println!("{:?} {}", choice, position.0);
     }
 }
 
