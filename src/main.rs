980df0468615180d0542e54275711a031283262a
Nathan Stocks <cleancut@github.com>
2021-04-18 22:04:34 -0600

    Update to Bevy 0.5.  Make a more interesting, visual demo.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
 use bevy::prelude::*;
 
-struct Velocity(f32);
-struct Position(f32);
-#[derive(Debug)]
-enum Choice {
-    Yes,
-    No,
-}
-
-// this system spawns entities with the Position and Velocity components
-fn setup(commands: &mut Commands) {
-    commands
-        .spawn((Position(0.0), Velocity(1.0), Choice::Yes))
-        .spawn((Position(1.0), Velocity(2.0), Choice::No));
-}
-
-// this system runs on each entity with a Position and Velocity component
-fn movement(mut query: Query<(&mut Position, &Velocity, &Choice)>) {
-    for (mut position, velocity, choice) in query.iter_mut() {
-        position.0 += velocity.0;
-        println!("{:?} {}", choice, position.0);
-    }
-}
-
-// the app entry point
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_system(movement.system())
+        .add_startup_system(template_setup.system())
+        .add_system(template_animation.system())
         .run();
+}
+
+fn template_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(Text2dBundle {
+        text: Text::with_section(
+            "Would you like to play a game?",
+            TextStyle {
+                font: asset_server.load("fonts/tiny.ttf"),
+                font_size: 58.0,
+                color: Color::WHITE,
+            },
+            TextAlignment {
+                vertical: VerticalAlign::Center,
+                horizontal: HorizontalAlign::Center,
+            },
+        ),
+        ..Default::default()
+    });
+}
+
+fn template_animation(time: Res<Time>, mut query: Query<&mut Transform, With<Text>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.x = 100.0 * time.seconds_since_startup().sin() as f32;
+        transform.translation.y = 100.0 * time.seconds_since_startup().cos() as f32;
+    }
 }
